bd2c201b5e63ea5e03ed9857dc57175ab6f9e451
Brecht Billiet <brecht.billiet@persgroep.be>
2015-10-05 16:35:15 +0200

    copy d.ts file to typings folder

--- dist/typescript-mockify.d.ts
+++ dist/typescript-mockify.d.ts
@@ -5,3 +5,54 @@ declare module 'typescript-mockify/mock/ConstructorArguments' {
 	}
 
 }
+declare module 'typescript-mockify/mock/StubbedFunc' {
+	import Spy = jasmine.Spy;
+	import { Mock } from 'typescript-mockify/mock/Mock';
+	export class StubbedFunc<T> {
+	    private func;
+	    private mock;
+	    constructor(func: Function, mock: Mock<T>);
+	    andReturn(value: any): Mock<T>;
+	    getSpy(): Spy;
+	}
+
+}
+declare module 'typescript-mockify/mock/Mock' {
+	import { ConstructorArguments } from 'typescript-mockify/mock/ConstructorArguments';
+	import { StubbedFunc } from 'typescript-mockify/mock/StubbedFunc';
+	export class Mock<T> {
+	    instance: T;
+	    args: ConstructorArguments;
+	    constructor(instance: T, args?: ConstructorArguments);
+	    setupMethod(func: string): StubbedFunc<T>;
+	    mapProperty(propertyName: string, value: any): Mock<T>;
+	}
+
+}
+declare module 'typescript-mockify/mock/IConstructor' {
+	export interface IConstructor<T> {
+	    prototype: any;
+	    apply(inst: any, args: Array<any>): void;
+	}
+
+}
+declare module 'typescript-mockify/mock/MockBuilder' {
+	import { Mock } from 'typescript-mockify/mock/Mock';
+	import { ConstructorArguments } from 'typescript-mockify/mock/ConstructorArguments';
+	import { IConstructor } from 'typescript-mockify/mock/IConstructor';
+	export class MockBuilder {
+	    static createInstance<T>(ctor: IConstructor<T>, args?: ConstructorArguments): Mock<T>;
+	    private static setDefaultVals(object, args?);
+	    private static createDefaultValue(type);
+	}
+
+}
+declare module 'typescript-mockify/index' {
+	/// <reference path="../typings/tsd.d.ts" />
+	export { MockBuilder } from 'typescript-mockify/mock/MockBuilder';
+	export { Mock } from 'typescript-mockify/mock/Mock';
+	export { StubbedFunc } from 'typescript-mockify/mock/StubbedFunc';
+	export { IConstructor } from 'typescript-mockify/mock/IConstructor';
+	export { ConstructorArguments } from 'typescript-mockify/mock/ConstructorArguments';
+
+}
