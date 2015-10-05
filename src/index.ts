d6c80596fe732bfd1ef0e715acd9436d8f1ea559
Brecht Billiet <brecht.billiet@persgroep.be>
2015-10-05 16:19:36 +0200

    fixed dist package

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 /// <reference path="../typings/tsd.d.ts" />
-import {MockBuilder} from  "./mock/MockBuilder";
-import {Mock} from  "./mock/Mock";
-import {StubbedFunc} from  "./mock/StubbedFunc";
-import {IConstructor} from  "./mock/IConstructor";
-import {ConstructorArguments} from  "./mock/ConstructorArguments";
-
-export {
-    MockBuilder,
-    Mock,
-    StubbedFunc,
-    IConstructor,
-    ConstructorArguments
-}
+export {MockBuilder} from  "./mock/MockBuilder";
+export {Mock} from  "./mock/Mock";
+export {StubbedFunc} from  "./mock/StubbedFunc";
+export {IConstructor} from  "./mock/IConstructor";
+export {ConstructorArguments} from  "./mock/ConstructorArguments";
--- tasks/dts-bundle.js
+++ tasks/dts-bundle.js
@@ -2,6 +2,8 @@ var dts = require('dts-generator');
 dts.generate({
     name: 'typescript-mockify',
     baseDir: 'src',
-    files: ['index.ts', 'mock/ConstructorArguments.ts', 'mock/IConstructor.ts', 'mock/Mock.ts', 'mock/MockBuilder.ts', 'mock/StubbedFunc.ts'],
+    files: [
+        'index.ts'
+    ],
     out: 'dist/typescript-mockify.d.ts'
 });
